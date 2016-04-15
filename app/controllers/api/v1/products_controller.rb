5b6777e3768fa0ebb285fe34cb6a9208158ae419
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2016-04-14 22:44:18 -0500

    Add endpoints PATCH and DELELTE products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ProductsController < ApplicationController
   protect_from_forgery with: :null_session
 
+  before_action :set_product, only: [:update, :destroy]
+
   def index
     render json: Product.all
   end
@@ -14,7 +16,23 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    if @product.update(product_params)
+      render json: product, status: :ok
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
   private
+    def set_product
+      @product = Product.find(params[:id])
+    end
     def product_params
       params.require(:product).permit(:name, :price)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :products, only: [:index, :create]
+      resources :products, only: [:index, :create, :update, :destroy]
     end
   end
 end
